68f9e70c5bdecd6d7f73d5049f074c81cdd2d7bc
Superior5 <superiorunknown5@gmail.com>
2023-11-04 10:05:31 +0300

    asdf

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 export default new Sequelize(
-    "postgresql://gen_user:z%5CGMh1Q-%2BdC%3B4w@188.225.44.61:5432/default_db",
+    "postgresql://gen_user:%26I_%3Fgr-~e%5E%23_s8@188.225.24.228:5432/default_db",
     {
         host: '188.225.44.61',
         port: '5432',
--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 import TelegramApi from 'node-telegram-bot-api';
 import sequelize from './db/db.js';
 import Test from './models/test.js'
+import cron from 'node-cron';
 const API_KEY_BOT = '6575266414:AAEs0X5Oxqoq8NHoAbwu479WrA2JwflMh-A';
 
 
 const bot = new TelegramApi(API_KEY_BOT, {polling: true});
 
 
+
 const start = async () => {
     
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-    } catch (error) {
-        console.log('Произошла ошибка при подлючении к БД', error)
-    }
     
     bot.setMyCommands([
         {command: '/start', description: 'Привет, я тупо сохраняю твои сообщения'},
@@ -22,18 +18,27 @@ const start = async () => {
         {command: '/show', description: 'Вывести историю сообщений'},
     ]);
 
+
     bot.on('message', async (msg)=>{
 
         const text = msg.text;
         const chatId = msg.chat.id;
 
+        
+
+
         if(msg.text === '/start') {
-            return bot.sendMessage(chatId,`Ну привет, ${msg.from.first_name} ${msg.from.last_name}! Я сохраняю твои сообщения.`);
+            return bot.sendMessage(chatId,`Привет, ${msg.from.first_name} ${msg.from.last_name}! Здесь можешь проверить статус своих Баз Данных.`);
         }
 
         if(msg.text === '/status') {
-            const info = await Test.findAll({chatId});
-            return bot.sendMessage(chatId,`Таблице Test ${info.length} записей!`);
+            try {
+                await sequelize.authenticate();
+                return bot.sendMessage(chatId,`База данных доступна!`);
+              } catch (error) {
+                console.log(error)
+                return bot.sendMessage(chatId,`‼️ Не удается подлючится как базе, данных! Проверьте настройки подключени или попробуйте восстановить её с контрольной точки.`);
+            }
         }
 
         if(msg.text === '/show') {
@@ -42,7 +47,9 @@ const start = async () => {
             return bot.sendMessage(chatId,`Таблице Test ${info} записей!`);
         }
         
-        await Test.create({chatId, text})
+
+   
+        // await Test.create({chatId, text})
         return bot.sendMessage(chatId,`Сообщение сохранено.`);
     });
 }
