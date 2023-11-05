9469beebf26acb865976e7e43b0dc34065be8037
Superior5 <superiorunknown5@gmail.com>
2023-11-05 06:48:16 +0300

    backup

--- db/db.js
+++ db/db.js
@@ -1,10 +1,13 @@
 import { Sequelize } from "sequelize";
 
 export default new Sequelize(
-  "postgresql://gen_user:%26I_%3Fgr-~e%5E%23_s8@188.225.24.228:5432/default_db",
-  {
-    host: "188.225.44.61",
-    port: "5432",
-    dialect: "postgres",
-  }
-);
+    "postgresql://gen_user:%26I_%3Fgr-~e%5E%23_s8@188.225.24.228:5432/default_db",
+    {
+        host: '188.225.44.61',
+        port: '5432',
+        dialect: 'postgres',
+    }
+)
+
+
+
--- main.js
+++ main.js
@@ -2,6 +2,33 @@ import TelegramApi from "node-telegram-bot-api";
 import sequelize from "./db/db.js";
 import Test from "./models/test.js";
 import cron from "node-cron";
+import  { execute } from '@getvim/execute';
+import shell from 'shelljs'
+
+import { execSync }  from "child_process";
+
+
+const test = 'pg_dump -h 188.225.24.228 -p 5432 -U gen_user default_db > C:/projects/backend/hackathonBot/backupsmyDump.sql'
+const execRun = (cmd) => {
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          if (error.code === 1) {
+            // leaks present
+            resolve(stdout);
+          } else {
+            // gitleaks error
+            reject(error);
+          }
+        } else {
+          // no leaks
+          resolve(stdout);
+        }
+      })
+    })
+  }
+
+
 const API_KEY_BOT = "6575266414:AAEs0X5Oxqoq8NHoAbwu479WrA2JwflMh-A";
 
 const bot = new TelegramApi(API_KEY_BOT, { polling: true });
@@ -14,6 +41,7 @@ const start = async () => {
     },
     { command: "/status", description: "Информация о состоянии базы данных" },
     { command: "/show", description: "Вывести историю сообщений" },
+    { command: "/backup", description: "Вывести историю сообщений" },
   ]);
 
   bot.on("message", async (msg) => {
@@ -63,9 +91,49 @@ const start = async () => {
       return bot.sendMessage(chatId, `Таблице Test ${info} записей!`);
     }
 
+
+    
+    if (msg.text === "/backup") {
+
+        // (async () => {
+        //     try {
+        //       const testing = await execRun(test)
+        //       console.log(testing)
+        //     } catch (e) {
+        //       console.log(e)
+        //     }
+        //     })()
+        
+
+        // const dump_result = execSync(test);
+        // console.log(dump_result.toString());
+        // exec("pg_dump -h 188.225.24.228 -p 5432 -U gen_user default_db > C:\projects\backend\hackathonBot\backupsmyDump.sql", (error, stdout, stderr) => {
+        //     if (error) {
+        //         console.log(`error: ${error.message}`);
+        //         return;
+        //     }
+        //     if (stderr) {
+        //         console.log(`stderr: ${stderr}`);
+        //         return;
+        //     }
+        //     return bot.sendMessage(chatId, `Бэкап сохранен`);
+
+        // });
+
+        execute('bash pg_dump -h 188.225.24.228 -p 5432 -U gen_user default_db > C:\projects\backend\hackathonBot\backups\myDump.sql')
+        .then(async () => {
+            return bot.sendMessage(chatId, `Бэкап сохранен`);
+        }).catch(err => {
+            console.log(err);
+        });
+        
+      }
+  
     // await Test.create({chatId, text})
     return bot.sendMessage(chatId, `Сообщение сохранено.`);
   });
 };
+
+
 
 start();
