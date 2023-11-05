267df24c38ce3d35ab47700990449daef53724ae
Superior5 <superiorunknown5@gmail.com>
2023-11-05 11:20:00 +0300

    fix

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ const start = async () => {
             chatId
         })
 
-        const test = `PGPASSWORD=&I_?gr-~e^#_s8 pg_dump -h ${HOST} -p ${PORT} -U gen_user default_db > backups/${fileName}`
+        const test = `PGPASSWORD="&I_?gr-~e^#_s8" pg_dump -h 188.225.24.228 -p 5432 -U gen_user default_db > backups/${fileName}`
 
         execute(test)
         .then(async () => {
